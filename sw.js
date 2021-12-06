ff20a003d8377ba1562699ec1977ae4a3455bc88
Cristian_Martinez <thealxzerox@gmail.com>
2021-12-06 15:55:02 -0600

    manifest, sw, icon & more...

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ const CACHE_NAME = 'v1_cache_AlxZeroX_Chronicles',
     'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
     './style.css',
     './script.js',
-    './img/ProgramadorFitness.png',
+    './img/AlxZeroXChronicles.png',
     './img/favicon.png'
   ]
 
